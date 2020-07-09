fc77a6f95ef6134d21f84ca2b52edcff44f6d969
francesco <fraleone99@gmail.com>
2020-07-09 21:18:42 +0200

    type of command

--- main.c
+++ main.c
@@ -1,28 +1,52 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
 #define MAX_DIM 1024
 
-typedef struct text{
+typedef struct texts{
    char string[MAX_DIM]= '';
    struct text* next;
    struct text* prec;
-}TextT;
+   int ind;
+}text;
+
+typedef struct commands{
+    int ind1;
+    int ind2;
+    char action;
+}command; //comandi c,d,p
+
+typedef struct undo_commands{
+    int n;
+    char action;
+}u_r; //comandi u r
 
 int main() {
-    char current_command[5] = '';
-    int ind1, ind2;
-    gets(current_command);
-    while(current commmand != 'q'){
-        if(current_command[1] != ','){
-            //TODO
-        }
-        else{
-            current_command[0] = ind1;
-            current_command[2] = ind1;
-        }
+    char command[8];
+    gets(command);
+    if(type_of_command(commmand) == 0){
+
     }
+    else if(type_of_command(commmand) == 1){
+
+    }
+
+
 }
 
-void change(int ind1, int ind2){}
-void print(int ind1, int ind2){}
-void delete(int ind1, int ind2){}
+int type_of_command(char* command){ //ritorna 0 se il tipo di comando è c,d,p. 1 altrimenti.
+
+    if(strchr(command, ',') == NULL){
+        return 0;
+    }
+    else{
+        return 1;
+    }
+
+}
+
+
+//void change(int ind1, int ind2){}
+//void print(int ind1, int ind2){}
+//void delete(int ind1, int ind2){}
